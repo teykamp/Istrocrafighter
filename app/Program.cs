4ac7d5f0b24a490beeec962b87306f640a2df1fb
Thomas <embergameslp@gmail.com>
2020-08-27 20:11:51 -0400

    Comments

--- app/Program.cs
+++ app/Program.cs
@@ -7,7 +7,9 @@ namespace app
         static void Main(string[] args){
 
             Console.Title = "Game Server";
+            // MaxPlayers, Port
             Server.Start(50, 2695);
+            // key press to end program
             Console.ReadKey();
             
         }
