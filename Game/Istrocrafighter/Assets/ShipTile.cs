283abfc243e2eaa2bf55ab17a3d51275c92b0cca
Zhi Zun Chen <beurryllmew@gmail.com>
2020-09-12 22:38:30 -0700

    Started some framework for representing ships. pogger

---
+++ Game/Istrocrafighter/Assets/ShipTile.cs
@@ -0,0 +1,17 @@
+﻿// ShipTile object that populates an 2d array to describe a ship
+public class ShipTile
+{
+    // Name of instance, possible external enum resource?
+    private string id = "";
+    // Arrays of ordered pairs 
+    private int[][] connectList = new int[][];
+    private int[][] adjacentList = new int[][];
+    private int hp = 0;
+    private int mass = 0;
+
+    // Default Constructor
+    public ShipTile()
+    {
+        
+    }
+}
